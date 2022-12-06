4b88a37f945c3908299663f2806649c77a019677
Ivan Alcalde <mail.ivan.ag@gmail.com>
2022-12-06 23:53:20 +0100

    Day 6 - Tunning Trouble (part 2) 📡 📶

--- lib/day_06/tunning_trouble.ex
+++ lib/day_06/tunning_trouble.ex
@@ -1,35 +1,39 @@
 defmodule Adventofcode2022.Day06.TunningTrouble do
-  def get_start_of_packet_marker(input) do
+
+  def get_start_of_packet_marker(input), do: get_marker(input, 4)
+  def get_start_of_message_marker(input), do: get_marker(input, 14)
+
+  defp get_marker(input, chunks_size) do
     buffer = String.graphemes(input)
     
     {_char, index} = buffer
     |> Enum.with_index
-    |> make_chunks_by_4()
+    |> make_chunks_by(chunks_size)
     |> Enum.find(&all_chars_different?/1)
     |> List.last()
 
     index + 1
   end
 
-  def all_chars_different?(chars_with_index) do
+  defp all_chars_different?(chars_with_index) do
     chars = Enum.map(chars_with_index, fn {char, _index} -> char end)
 
     length(chars) == length(Enum.uniq(chars))
   end
 
-  def make_chunks_by_4(xs, chunks \\ [])
+  defp make_chunks_by(xs, chunk_size, chunks \\ [])
 
-  def make_chunks_by_4([_a, _b, _c, _d] = chunk, chunks) do
-    chunks ++ [chunk]
+  defp make_chunks_by(xs, chunk_size, chunks) when length(xs) == chunk_size do
+    chunks ++ [xs]
   end
 
-  def make_chunks_by_4(xs, chunks) do
-    chunk = Enum.slice(xs, 0, 4)
+  defp make_chunks_by(xs, chunk_size, chunks) do
+    chunk = Enum.slice(xs, 0, chunk_size)
 
     chunks = chunks ++ [chunk]
 
     [_head | tail] = xs
 
-    make_chunks_by_4(tail, chunks)
+    make_chunks_by(tail, chunk_size, chunks)
   end
 end
--- test/day_06/tunning_trouble_test.exs
+++ test/day_06/tunning_trouble_test.exs
@@ -15,4 +15,18 @@ defmodule Adventofcode2022.Day06.TunningTroubleTest do
 
     assert TunningTrouble.get_start_of_packet_marker(input) == 1833
   end
+
+  test "returns the right start of message marker" do
+    assert TunningTrouble.get_start_of_message_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 19
+    assert TunningTrouble.get_start_of_message_marker("bvwbjplbgvbhsrlpgdmjqwftvncz") == 23
+    assert TunningTrouble.get_start_of_message_marker("nppdvjthqldpwncqszvftbrmjlhg") == 23
+    assert TunningTrouble.get_start_of_message_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 29
+    assert TunningTrouble.get_start_of_message_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 26
+  end
+
+  test "returns the right start of message marker (puzzle input)" do
+    input = File.read!(Path.join(__DIR__, "./input.txt"))
+
+    assert TunningTrouble.get_start_of_message_marker(input) == 3425
+  end
 end
