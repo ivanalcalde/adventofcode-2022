fc6b5a2d929f4c6db132f7572fa70434868632bf
Ivan Alcalde <mail.ivan.ag@gmail.com>
2022-12-07 12:14:02 +0100

    Refactor - Day 6 - Tunning Trouble (part 2) 📡 📶

--- lib/day_06/tunning_trouble.ex
+++ lib/day_06/tunning_trouble.ex
@@ -3,37 +3,24 @@ defmodule Adventofcode2022.Day06.TunningTrouble do
   def get_start_of_packet_marker(input), do: get_marker(input, 4)
   def get_start_of_message_marker(input), do: get_marker(input, 14)
 
-  defp get_marker(input, chunks_size) do
+  def get_marker(input, chunks_size) do
     buffer = String.graphemes(input)
-    
-    {_char, index} = buffer
-    |> Enum.with_index
-    |> make_chunks_by(chunks_size)
-    |> Enum.find(&all_chars_different?/1)
-    |> List.last()
 
-    index + 1
+    start_marker_index = 
+      0..length(buffer)-1
+      |> Enum.take_while(fn index ->
+        not (Enum.slice(buffer, index, chunks_size) |> all_chars_different?())
+      end)
+      |> List.last()
+
+    if start_marker_index == nil do
+      nil
+    else
+      start_marker_index + 1 + chunks_size
+    end
   end
 
-  defp all_chars_different?(chars_with_index) do
-    chars = Enum.map(chars_with_index, fn {char, _index} -> char end)
-
+  defp all_chars_different?(chars) do
     length(chars) == length(Enum.uniq(chars))
-  end
-
-  defp make_chunks_by(xs, chunk_size, chunks \\ [])
-
-  defp make_chunks_by(xs, chunk_size, chunks) when length(xs) == chunk_size do
-    chunks ++ [xs]
-  end
-
-  defp make_chunks_by(xs, chunk_size, chunks) do
-    chunk = Enum.slice(xs, 0, chunk_size)
-
-    chunks = chunks ++ [chunk]
-
-    [_head | tail] = xs
-
-    make_chunks_by(tail, chunk_size, chunks)
   end
 end
